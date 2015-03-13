e36127717e8cc5cdb44d44ce6517abdd9b35eef7
Vincent Listrani <vincent@3five.com>
2015-03-13 12:58:05 -0500

    configuration tests

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@
 ############################################
 
 set :stage, :production
-set :stage_url, "http://wpdeploytest.io"
+set :stage_url, "http://rudimentsstack.3fivelab.com"
 server "104.130.201.40", user: "deploy", roles: %w{web app db}
-set :deploy_to, "/var/www/vhosts/wpdeploytest.io"
+set :deploy_to, "/var/www/vhosts/wpdeploytest.io/htdocs"
 
 ############################################
 # Setup Git
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 ############################################
 
 set :stage, :staging
-set :stage_url, "http://staging.wpdeploytest.io"
+set :stage_url, "http://staging.rudimentsstack.3fivelab.com"
 server "104.130.201.40", user: "deploy", roles: %w{web app db}
 set :deploy_to, "/var/www/vhosts/staging.wpdeploytest.io/htdocs"
 
